5159df0255789bf254b91cce79c4aa242843d44a
Jorge <jorgeluisas6@gmail.com>
2024-10-11 13:34:49 -0600

    testing

---
+++ src/lab1p2q4/Lab1P2Q4.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lab1p2q4;
+
+/**
+ *
+ * @author Jorge Aguirre
+ */
+public class Lab1P2Q4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
